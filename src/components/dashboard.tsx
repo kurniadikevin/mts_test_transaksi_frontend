2cb88e4ec5906a1854ffcce930ce27e56a6c88a5
kurniadikevin <kurniadikevin@gmail.com>
2023-06-26 16:12:03 +0700

    add current username on dashboard

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -2,12 +2,25 @@ import 'material-icons/iconfont/material-icons.css';
 import { useRouter } from 'next/navigation';
 import Modal from './modal';
 import Loader from './loader/loader';
+import { useEffect, useState } from 'react';
 
 
 
 export default function Dashboard(){
 
     const { push } = useRouter();
+    const [currentUser,setCurrentUser]=useState<any>();
+
+    const getCurrentUser=()=>{
+        const user :any= localStorage.getItem('session-data');
+        if(user){
+            setCurrentUser(JSON.parse(user))
+        }
+    }
+ 
+    useEffect(()=>{
+       getCurrentUser()
+    },[])
 
     return(
         <div id="dashboard-component" className=" pl-8 pr-4">
@@ -38,6 +51,10 @@ export default function Dashboard(){
                 <div id='dash-select' className="bg-[color:var(--button)] w-30 h-15 p-2 
                 rounded-2xl  flex justify-start items-center">
                     Sign-out</div>
+            </div>
+            <div id='dash-select'
+             className="flex justify-start pl-10  items-center font-bold text-xl text-[color:var(--button)]">
+                 {currentUser?.username}
             </div>
             <Modal />
             <Loader/>
